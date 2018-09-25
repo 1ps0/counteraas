7f755b0c4cb101d7da4bf0211354a3f5f7e7a21e
Alex Evers <alex.evers2@gmail.com>
2018-09-24 22:48:20 -0700

    Added the missing port :D

--- server.py
+++ server.py
@@ -41,4 +41,5 @@ def inc_counter():
     return jsonify({ 'status': validate })
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
